f8a37e209f28dc2e35a6aa1833d330b4befe0532
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-13 13:48:03 -0700

    Add create,update burger list on index.handlebar

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Chesse Burgers", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Ham Burgers", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegetarian Burgers", false);
+INSERT INTO burgers (name) VALUES ("Chesse Burgers");
+INSERT INTO burgers (name) VALUES ("Ham Burgers");
+INSERT INTO burgers (name) VALUES ("Vegetarian Burgers");
--- models/burger.js
+++ models/burger.js
@@ -16,9 +16,14 @@ var burger = {
       orm.update("burgers", objColVals, condition, function(res) {
         cb(res);
       });
+    },
+    delete: function (condition, cb) {
+      orm.delete("burgers", condition, function (res) {
+          cb(res);
+      });
     }
   };
   
-  // Export the database functions for the controller (catsController.js).
+  // Export the database functions for the controller (burgersController.js).
   module.exports = burger;
   
