e451d122ffbb88537281b107095bf44a98798d53
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-13 23:54:06 -0700

    Added burger img, devour button,reorder to burger-block

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,7 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
     $(".change-devoured").on("click", function (event) {
+       
         var id = $(this).data("id");
         var newDevoured = $(this).data("newdevoured");
 
@@ -58,3 +59,7 @@ $(".delete-burger").on("click", function (event) {
         }
     );
 });
+
+$('.carousel').carousel({
+    interval: 2000
+  });
