604728c66c676236e525a008c1ccd3f948c847dd
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-16 21:06:21 -0700

    Add on mysql

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "ben12345",
-  database: "burgers_db"
+  user: "rbjio6i767nnkcga",
+  password: "pqmx5ev31s3q6up1",
+  database: "	rb2nw93s1mwhhiaj"
 });
 
 // Make connection.
