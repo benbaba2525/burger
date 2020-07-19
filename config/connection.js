fd4402da2e9cde56051c3fa3c9a61a5fef6855d7
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-18 19:49:15 -0700

    Add .env

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,25 @@
-// Set up MySQL connection.
+// require mysql npm package
+require('dotenv').config()
 var mysql = require("mysql");
-require('dotenv').config();
-
-var connection = mysql.createConnection({
-  host: process.env.host,
-  port: 3306,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
-});
-
-// Make connection.
+var connection;
+// if jaws db is present, connect to jaws db, otherwise connect to local mysql db
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.host, 
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+  });
+};
+// connect to database, throw error if error, otherwise log the connection id
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("error connecting to database: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected to database as id " + connection.threadId);
 });
-
-// Export connection for our ORM to use.
+// export connection for use in orm
 module.exports = connection;
